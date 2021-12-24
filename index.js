6ae7679fe3c3f5c4cc0bec461893ad5ef36a3e9f
DominiqueFlorence <ddaniell1987@gmail.com>
2021-12-24 14:09:32 -0500

    commit tree 59571f6ff1ea7ed8bbe5ad6b847079bef1f27977

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+const body = document.getElementsByTagName("body")[0];
+function submitData (firstName, email) {
+    const configurationObject={
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        "name": firstName,
+        "email": email
+
+    })
+}
+
+    fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(body =>addContact(body.id))
+    .catch(function(error){
+        alert("Unauthorized Access")
+    })
+}
+function addContact(newId){
+    document.getElementsByTagName("body")[0].innerHTML = newId;
+} 
+submitData("name", "name@name.com")
+
+
