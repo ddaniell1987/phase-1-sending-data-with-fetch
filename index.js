e19a1f412ab158bd8011931f6b47a3295d5966f8
DominiqueFlorence <ddaniell1987@gmail.com>
2021-12-27 20:00:02 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+const body = document.getElementsByTagName("body")[0];
+function submitData(firstName, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    }
+   return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(body =>addNewContact(body.id))
+    .catch(response => 'Unauthorized Access')
+}
+function addNewContact(newId){
+    document.getElementsByTagName("body")[0].innerHTML = newId;
+    document.body.append('Unauthorized Access');
+
+}
+submitData("name", "name@name.com");
